7aa30444f5e849a2ff1c2c4397a0569597f638cc
James Rogers <jarogers095@gmail.com>
2020-04-14 07:34:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,10 +21,10 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
     params.map do |k, v|
       v
-    end.join(" ") << "."
+    end.join(" ").push(".")
   end
   
   get "/:operation/:number1/:number2" do 
-    
+    Math.send("#{params[:operation]}", params[:number1], params[:number2]).to_s
   end
 end
