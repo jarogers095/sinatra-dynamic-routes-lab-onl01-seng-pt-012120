6a907569496a7c0c5b3ffcd5aeaea3aff73c828a
James Rogers <jarogers095@gmail.com>
2020-04-14 07:31:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,9 @@ class App < Sinatra::Base
   end
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
-    
+    params.map do |k, v|
+      v
+    end.join(" ") << "."
   end
   
   get "/:operation/:number1/:number2" do 
