9ba13e3d6415e29e1ca080c385d9355c0253a42f
James Rogers <jarogers095@gmail.com>
2020-04-14 07:40:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,10 +25,18 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do 
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+
     case params[:operation]
     when "add"
-      return
+      "#{n1 + n2}"
     when "subtract"
-        
+      "#{n1 - n2}"
+    when "multiply"
+      "#{n1 * n2}"
+    when "divide"
+      "#{n1 / n2}"
+    end
   end
 end
